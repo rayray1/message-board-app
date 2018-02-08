ba3599b4baada1adf995d603be8611cb66172a9d
Raymond Rupiah <rupiray@yahoo.com>
2018-02-08 11:30:29 +0000

    files added

--- board_project/urls.py
+++ board_project/urls.py
@@ -1,4 +1,4 @@
-"""helloworld_project URL Configuration
+"""board_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-
 from . import views
 
 
 urlpatterns = [
-    path('', views.homePageView, name='home')
+    path('', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
 ]
