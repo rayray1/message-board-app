ace61029deee6e84f097087c5e5a26766cddcd29
Raymond Rupiah <rupiray@yahoo.com>
2018-02-08 20:35:23 +0000

    added new functionalities

--- board_project/urls.py
+++ board_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
+    path('', include('blog.urls')),
+    # path('', include('posts.urls')),
     # path('', include('pages.urls')),
 ]
